7c4df42008b0f8e1677e29d23c892fd1b886c574
Shardul Srivastava <shardulsrivastava@users.noreply.github.com>
2024-10-06 18:51:53 +0000

    Add repo permissions

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,18 @@ docker build -f backend/Dockerfile backend/
 
 echo "Running Backend"
 docker run backend
+
+# Check out Repository
+
+# Skip smudge - We'll download binary files later in a faster batch
+# git lfs install --skip-smudge
+
+# Do git clone here
+# git clone ...
+
+# Fetch all the binary files in the new clone
+# git lfs pull
+
+# Reinstate smudge
+# git lfs install --force
+# git lfs pull --include model_zoo/ECG2AF/ecg_5000_survival_curve_af_quadruple_task_mgh_v2021_05_21.h5
