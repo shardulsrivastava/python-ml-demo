de37a1539a1b50391dece4b7b30932f5a3958c30
Shardul Srivastava <shardulsrivastava@users.noreply.github.com>
2024-10-06 21:13:38 +0000

    Create backend files

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi import File
 from fastapi import FastAPI
 from fastapi import UploadFile
 from PIL import Image
+from prediction import predict
 
 app = FastAPI()
 
@@ -13,9 +14,10 @@ def read_root():
     return {"message": "Welcome to ECG Intelligence"}
 
 @app.post("/predict")
-def get_image(file: UploadFile = File(...)):
-    pass
+# def get_prediction(file: UploadFile = File(...)):
+def get_prediction():
+    predict()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",port=8080)
-    
+    uvicorn.run("main:app", host="0.0.0.0",port=8081)
+     
--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,13 @@ echo "Building Backend"
 docker build -f backend/Dockerfile backend/
 
 echo "Running Backend"
-docker run backend
+docker run --rm -p 8081:8081 backend 
+
+echo "Testing the application"
+curl --header "Content-Type: application/json" \
+        --request POST \
+        --data '{"username":"xyz","password":"xyz"}' \
+        https://ideal-barnacle-ggxpw59g7wfvwv4-8081.app.github.dev/predict
 
 # Check out Repository
 # cd ../ && git lfs install --skip-smudge
