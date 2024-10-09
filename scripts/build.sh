3681777b1bd3a16114b15c702c9338357f5ad171
Shardul Srivastava <shardul.srivastava007@gmail.com>
2024-10-09 02:20:05 +0200

    Add automatic run of script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,22 +11,22 @@ set -euo pipefail
 # cd /workspaces; git clone git@github.com:broadinstitute/ml4h.git
 # cp ml4h/model_zoo/ECG2AF/ecg_5000_survival_curve_af_quadruple_task_mgh_v2021_05_21.h5 python-ml-demo/backend
 
-echo "Building Backend"
-docker build -f backend/Dockerfile backend/
-
-echo "Running Backend"
-docker run --rm -p 8081:8081 backend:v1
+#echo "Building Backend"
+#docker build -f backend/Dockerfile backend/
+#
+#echo "Running Backend"
+#docker run --rm -p 8081:8081 backend:v1
 
 # echo "Testing the application"
-curl --header "Content-Type: application/json" \
-        --request POST \
-        --data '{"username":"xyz","password":"xyz"}' \
-        https://ubiquitous-sniffle-px76qgxr6rh6g6-8081.app.github.dev/test
-
-curl -v --header "Content-Type: application/json" \
-        --request POST \
-        --data '{"username":"xyz","password":"xyz"}' \
-        http://localhost:8081/test
+#curl --header "Content-Type: application/json" \
+#        --request POST \
+#        --data '{"username":"xyz","password":"xyz"}' \
+#        https://ubiquitous-sniffle-px76qgxr6rh6g6-8081.app.github.dev/test
+#
+#curl -v --header "Content-Type: application/json" \
+#        --request POST \
+#        --data '{"username":"xyz","password":"xyz"}' \
+#        http://localhost:8081/test
 
 # Check out Repository
 # cd ../ && git lfs install --skip-smudge
@@ -37,3 +37,9 @@ curl -v --header "Content-Type: application/json" \
 
 # Skip smudge - We'll download binary files later in a faster batch
 # # git lfs install --force
+
+echo "Building Images"
+docker-compose build
+
+echo "Running Application"
+docker-compose up
