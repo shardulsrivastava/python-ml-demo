35b032a69c327b3c3ff7e02d7fc09134e2fa3260
Shardul Srivastava <shardul.srivastava007@gmail.com>
2024-10-09 14:56:11 +0200

    Add Regex to allow CORS from Codespaces

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ origins = ["http://localhost:8080"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex='https://.*\.app\.github\.dev',
     allow_methods=["*"],
     allow_headers=["*"],
 )
